4b950ff0c4d4f944201bcd546e5dccabdebeea3f
Pradeek444 <113882354+Pradeek444@users.noreply.github.com>
2024-03-05 14:39:10 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/jobapplication/pradeek/dto/response/ForgotPasswordResponse.java
@@ -0,0 +1,14 @@
+package com.jobapplication.pradeek.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordResponse {
+ @Builder.Default
+ private String message="Something went wrong";
+}
